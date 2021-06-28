095ebf56128d0d60b53690ccfb220652075f646e
Panagiotis.Kolaitis <panagiotis.kolaitis.ext@proximus.com>
2021-06-28 19:01:27 +0200

    moving on

--- banking-app/src/app/app-routing.module.ts
+++ banking-app/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ActionsComponent } from './components/actions/actions.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { TransactionsComponent } from './components/transactions/transactions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '/', component: LandingComponent},
+  {path : '/landing', component: LandingComponent},
+  {path : '/login', component: LoginComponent},
+  {path : '/logout', component: LogoutComponent},
+  {path : '/profile', component: ProfileComponent},
+  {path : '/reset', component: ResetPasswordComponent},
+  {path : '/transactions', component: TransactionsComponent},
+  {path : '/actions', component: ActionsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- banking-app/src/app/app.module.ts
+++ banking-app/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { TransactionsComponent } from './components/transactions/transactions.component';
+import { ActionsComponent } from './components/actions/actions.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    LoginComponent,
+    LogoutComponent,
+    ResetPasswordComponent,
+    ProfileComponent,
+    TransactionsComponent,
+    ActionsComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
